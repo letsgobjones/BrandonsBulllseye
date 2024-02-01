832fc5a82670dbdf4809645010ce89380abb9030
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-02-01 23:35:44 +0900

    Added a Reset Button, Bonus points feature, and Unit testing for the features.

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -15,8 +15,45 @@ struct Game {
   var round = 1
   
   func point(sliderValue: Int) -> Int {
-    100 - abs(target - sliderValue)
+    
+    let bonus: Int
+    let difference = abs(target - sliderValue)
+
+    
+    if difference == 0 {
+      
+      bonus = 100
+    } else if   difference <= 2 {
+      bonus = 50
+    
+    }
+    else {
+      bonus = 0
+      
+    }
+    return 100 - difference + bonus
+
+    
   }
   
+  
+  mutating func startNewRound(points: Int) {
+    
+    
+    score += points
+    round += 1
+  target = Int.random(in: 1...100)
+    
+  }
+
+  
+  
+  mutating func restart() {
+    
+    round = 1
+    score = 0
+    target = Int.random(in: 1...100)
+
+  }
   
 }
--- BullseyeTests/BullseyeTests.swift
+++ BullseyeTests/BullseyeTests.swift
@@ -32,4 +32,40 @@ final class BullseyeTests: XCTestCase {
     XCTAssertEqual(score, 95)
   }
   
+
+  
+  func testScoreExact() {
+    var guess = game.target
+    var score = game.point(sliderValue: guess)
+    XCTAssertEqual(score, 200)
+  }
+  
+  func testScoreClose() {
+    var guess = game.target + 2
+    var score = game.point(sliderValue: guess)
+    XCTAssertEqual(score, 98 + 50)
+  }
+  
+  
+  func testNewRound() {
+    game.startNewRound(points: 100)
+    XCTAssertEqual(game.score, 100)
+    XCTAssertEqual(game.round, 2)
+  }
+  
+  
+  func testRestart() {
+    game.startNewRound(points: 100)
+    XCTAssertNotEqual(game.score, 0)
+    XCTAssertNotEqual(game.round, 1)
+    game.restart()
+    XCTAssertEqual(game.score, 0)
+    XCTAssertEqual(game.round, 1)
+    
+    
+    
+  }
+  
+  
+  
 }
